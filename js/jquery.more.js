12674a594ec278af3c074616fdb8717b9304fd34
程刁 <diaocheng@outlook.com>
2016-06-25 18:09:07 +0800

    优化js代码

--- js/jquery.more.js
+++ js/jquery.more.js
@@ -9,7 +9,7 @@
  * }
  * @param  boolean options.disabled 可选参数(默认为false)
  * @return integer options.offset  当绑定为window时为可选参数(默认为0)
- * callback为回调函数 more返回的为插件的disabled,animate.target和animate.interval参数对象
+ * callback为回调函数 more返回的为插件的set函数对象，more.set()参数为true或false
  */
 (function($) {
     'use strict';
@@ -21,15 +21,28 @@
             if (typeof(options.object) == "undefined") {
                 throw new Error('未绑定对象');
             }
-            if(typeof(options.animate)=="undefined"){
-                options.animate=$("#more[data-target='more']")
+            if (typeof(options.animate) == "undefined") {
+                options.animate = $(".more[data-target='more']")
             }
             if (typeof(options.disabled) == "undefined") {
                 options.disabled = false;
             }
+            if (typeof(options.cycle) == "undefined") {
+                options.cycle = 4;
+            }
+            if (typeof(options.frequency) == "undefined") {
+                options.frequency = 1000;
+            }
+            //定义内部引用参数
             this.options = {
                 disabled: options.disabled,
-            };
+                target: options.animate,
+                cycle: options.cycle,
+                frequency: options.frequency,
+                animateOn: "加载中",
+                animateOff: "加载更多",
+                animateOver: "没有更多了"
+            }
             if (options.object[0] == window) {
                 if (typeof(options.offset) == "undefined") {
                     options.offset = 0;
@@ -53,9 +66,9 @@
                             more.options.disabled = true;
                             options.scrollTop = options.object.container.scrollTop();
                             //防止不能触发滚动
-                            options.object.container.scrollTop(options.scrollTop - 5);
-                            more.loading(options.animate);
-                            return options.callback(more.options.animate);
+                            options.object.container.scrollTop(options.scrollTop - 1);
+                            more.animate();
+                            return options.callback(more.set);
                         }
                     }
                 }
@@ -65,29 +78,32 @@
             options.object.on("click", function() {
                 if (!more.options.disabled) {
                     more.options.disabled = true;
-                    more.loading(options.animate);
-                    return options.callback(more.options.animate);
+                    more.animate();
+                    return options.callback(more.set);
                 }
             });
         },
-        loading: function(options) {
+        animate: function() {
             var count = 0;
-            var text = "加载中";
-            options.html(text);
-            more.options.animate = {
-                target: options,
-                interval: setInterval(function() {
-                    count++;
-                    text += ".";
-                    if (count % 4 == 0) {
-                        text = "加载中";
-                    }
-                    options.html(text);
-                }, 1000),
-                stop:function(options){
-                    clearInterval(more.options.animate.interval);
-                    more.options.disabled=options.disabled;
-                    more.options.animate.target.html(options.text);
+            var showTxt = more.options.animateOn;
+            more.options.target.html(showTxt);
+            more.options.interval = setInterval(function() {
+                count++;
+                showTxt += ".";
+                if (count % more.options.cycle == 0) {
+                    showTxt = more.options.animateOn;
+                }
+                more.options.target.html(showTxt);
+            }, more.options.frequency)
+        },
+        set: {
+            disabled: function(disabled) {
+                clearInterval(more.options.interval);
+                more.options.disabled = disabled;
+                if (!disabled) {
+                    more.options.target.html(more.options.animateOff);
+                } else {
+                    more.options.target.html(more.options.animateOver);
                 }
             }
         }
